4e8864e62c11c90d012cb11431701f9e9b9ceaad
Kyle Pugh <kyletpugh@users.noreply.github.com>
2020-03-06 20:07:09 -0500

    Fix client and runtime tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,11 @@
 """Client application unit testing"""
 
+import os
+import sys
+sys.path.insert(0, os.path.dirname(sys.path[0]) + os.sep + 'src' + os.sep + 'internal')
+
+from meg_runtime import Config, Plugin, PluginManager
+
 def test_client():
     """TODO: Test client"""
     pass
